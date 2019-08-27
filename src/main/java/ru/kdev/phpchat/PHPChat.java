8a94b4e8c794f61580ab38ebddc577d5bc3b5c5b
Konstantin <dropinclass1@gmail.com>
2019-08-27 19:59:25 +0500

    Update PHPChat.java

--- src/main/java/ru/kdev/phpchat/PHPChat.java
+++ src/main/java/ru/kdev/phpchat/PHPChat.java
@@ -7,10 +7,7 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLEncoder;
+import java.net.*;
 
 public class PHPChat extends JavaPlugin implements Listener {
     @Override
@@ -25,31 +22,86 @@ public class PHPChat extends JavaPlugin implements Listener {
     }
 
     @EventHandler
-    public void onChat(AsyncPlayerChatEvent e) throws IOException {
-        StringBuffer sb = null;
-        String buildedMsg = e.getPlayer().getName() + ": " + e.getMessage();
-        String data = URLEncoder.encode("message", "UTF-8") + "="
-                + URLEncoder.encode(buildedMsg, "UTF-8");
-        URL url = new URL(getConfig().getString("url"));
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setDoOutput(true);
-        conn.setRequestMethod("POST");
-        OutputStreamWriter osw = new OutputStreamWriter(
-                conn.getOutputStream());
-        osw.write(data);
-        osw.flush();
+    public void onChat(final AsyncPlayerChatEvent e) throws IOException {
+        Bukkit.getScheduler().runTaskAsynchronously(this, new Runnable() {
+            public void run() {
+                StringBuffer sb = null;
+                String buildedMsg = e.getPlayer().getName() + ": " + e.getMessage();
+                String data = null;
+                try {
+                    data = URLEncoder.encode("message", "UTF-8") + "="
+                            + URLEncoder.encode(buildedMsg, "UTF-8");
+                } catch (UnsupportedEncodingException ex) {
+                    ex.printStackTrace();
+                }
+                URL url = null;
+                try {
+                    url = new URL(getConfig().getString("url"));
+                } catch (MalformedURLException ex) {
+                    ex.printStackTrace();
+                }
+                HttpURLConnection conn = null;
+                try {
+                    conn = (HttpURLConnection) url.openConnection();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+                assert conn != null;
+                conn.setDoOutput(true);
+                try {
+                    conn.setRequestMethod("POST");
+                } catch (ProtocolException ex) {
+                    ex.printStackTrace();
+                }
+                OutputStreamWriter osw = null;
+                try {
+                    osw = new OutputStreamWriter(
+                            conn.getOutputStream());
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+                try {
+                    osw.write(data);
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+                try {
+                    osw.flush();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(
-                conn.getInputStream()));
+                BufferedReader br = null;
+                try {
+                    br = new BufferedReader(new InputStreamReader(
+                            conn.getInputStream()));
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
 
-        String in = "";
-        sb = new StringBuffer();
+                String in = "";
+                sb = new StringBuffer();
 
-        while ((in = br.readLine()) != null) {
-            sb.append(in).append("\n");
-        }
+                while (true) {
+                    try {
+                        if (!((in = br.readLine()) != null)) break;
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                    sb.append(in).append("\n");
+                }
 
-        osw.close();
-        br.close();
+                try {
+                    osw.close();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+                try {
+                    br.close();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
     }
 }
